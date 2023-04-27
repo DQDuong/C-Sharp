b37608b53403f92edd7c6e06b9c2d47f6f400413
DoQDuong <110405690+DoQDuong@users.noreply.github.com>
2023-04-27 08:34:24 +0700

    27/04

--- T2207A/Program.cs
+++ T2207A/Program.cs
@@ -6,73 +6,124 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello world");
-        int x = 10;
-        string s = "T2207A";
-        double pi = 3.14159;
-        if (x > 5)
+        Studentmanagement quanLySinhVien = new Studentmanagement();
+        int chucNang = 0;
+        do
         {
-            x--;
-        }
-        else
-        {
-            x++;
-        }
-        int[] arr = new int[5];
-        arr[0] = 1;
-        arr[1] = 15;
-        arr[2] = 9;
-        arr[3] = 55;
-        arr[4] = -16;
+            Console.WriteLine("Chon chuc nang:");
+            Console.WriteLine("1. Them sinh vien");
+            Console.WriteLine("2. Cap nhat thong tin sv boi ID");
+            Console.WriteLine("3. Xoa sv boi ID");
+            Console.WriteLine("4. Tim kiem sv theo ten");
+            Console.WriteLine("5. Sap xep sv theo diem trung binh (GPA)");
+            Console.WriteLine("6. Sap xep sv theo ten");
+            Console.WriteLine("7. Sap xep sv theo ID");
+            Console.WriteLine("8. Hien thi danh sach sv");
+            Console.WriteLine("0. Thoat");
+            Console.Write("Chon chuc nang: ");
+            chucNang = int.Parse(Console.ReadLine());
+            switch (chucNang)
+            {
+                case 1:
+                    quanLySinhVien.ThemSinhVien();
+                    break;
+                case 2:
+                    quanLySinhVien.CapNhatSinhVien();
+                    break;
+                case 3:
+                    quanLySinhVien.XoaSinhVien();
+                    break;
+                case 4:
+                    quanLySinhVien.TimKiemSinhVien();
+                    break;
+                case 5:
+                    quanLySinhVien.SapXepTheoDiemTrungBinh();
+                    break;
+                case 6:
+                    quanLySinhVien.SapXepTheoTen();
+                    break;
+                case 7:
+                    quanLySinhVien.SapXepTheoID();
+                    break;
+                case 8:
+                    quanLySinhVien.HienThiDanhSachSinhVien();
+                    break;
+                case 0:
+                    Console.WriteLine("Ket thuc");
+                    break;
+                default:
+                    Console.WriteLine("Chuc nang khong ton tai!");
+                    break;
+            }
+            Console.WriteLine();
+        } while (chucNang != 0);
+    }
+}
+    //Console.WriteLine("Hello world");
+    //    int x = 10;
+    //    string s = "T2207A";
+    //    double pi = 3.14159;
+    //    if (x > 5)
+    //    {
+    //        x--;
+    //    }
+    //    else
+    //    {
+    //        x++;
+    //    }
+    //    int[] arr = new int[5];
+    //    arr[0] = 1;
+    //    arr[1] = 15;
+    //    arr[2] = 9;
+    //    arr[3] = 55;
+    //    arr[4] = -16;
 
-        foreach (int i in arr)
-        {
-            Console.WriteLine(i);
-        }
-        string cmd = Console.ReadLine();
-        Console.WriteLine("Chuoi vua nhap: "+cmd);
-        string cmd2 = Console.ReadLine();
-        int n = Convert.ToInt32(cmd2);
-        Console.WriteLine("So vua nhap: " + n);
-        if ( isPrime(n) )
-        {
-            Console.WriteLine(n+ "la so nguyen to");
-        }
-        List<int> ints = new List<int>();
-        ints.Add(4);
-        ints.Add(19);
+    //    foreach (int i in arr)
+    //    {
+    //        Console.WriteLine(i);
+    //    }
+    //    string cmd = Console.ReadLine();
+    //    Console.WriteLine("Chuoi vua nhap: "+cmd);
+    //    string cmd2 = Console.ReadLine();
+    //    int n = Convert.ToInt32(cmd2);
+    //    Console.WriteLine("So vua nhap: " + n);
+    //    if ( isPrime(n) )
+    //    {
+    //        Console.WriteLine(n+ "la so nguyen to");
+    //    }
+    //    List<int> ints = new List<int>();
+    //    ints.Add(4);
+    //    ints.Add(19);
 
-        Student st = new Student();
-        st.Run();
-        st.Learn("ABC");
-        Console.WriteLine(st.Name);
-        st.Name = "Nguyen Van A";
+    //    Student st = new Student();
+    //    st.Run();
+    //    st.Learn("ABC");
+    //    Console.WriteLine(st.Name);
+    //    st.Name = "Nguyen Van A";
 
-        Human h = new Human();
-        Human h2 = new Human() { Telephone = "09876543"};
+    //    Human h = new Human();
+    //    Human h2 = new Human() { Telephone = "09876543"};
 
         
 
 
-    }
-    public static bool isPrime(int n)
-    {
-        if (n < 2) return false;
-        if (n < 4) return true;
-        for (int i = 2; i <= n/2; i++) { 
-        if (n % i == 0) return false;
-        }
-        return true;
+    //public static bool isPrime(int n)
+    //{
+    //    if (n < 2) return false;
+    //    if (n < 4) return true;
+    //    for (int i = 2; i <= n/2; i++) { 
+    //    if (n % i == 0) return false;
+    //    }
+    //    return true;
 
-        News news = new News();
-        news.Id = 1;
-        news.Title = "Hello World";
-        news.PublishDate = "21-04-2023";
-        news.Author = "Do Quang Duong";
-        news.Content = "This is an example news.";
-        int[] rateList = { 3, 4, 5 };
-        news.Calculate(rateList);
-        news.Display();
-    }
+    //    News news = new News();
+    //    news.Id = 1;
+    //    news.Title = "Hello World";
+    //    news.PublishDate = "21-04-2023";
+    //    news.Author = "Do Quang Duong";
+    //    news.Content = "This is an example news.";
+    //    int[] rateList = { 3, 4, 5 };
+    //    news.Calculate(rateList);
+    //    news.Display();
+    //}
     
-}
---
+++ T2207A/Studentmanagement.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace T2207A
+{
+    public class Studentmanagement
+    {
+        private List<Students> danhSachSinhVien;
+
+        public Studentmanagement()
+        {
+            danhSachSinhVien = new List<Students>();
+        }
+
+        public void ThemSinhVien()
+        {
+            Console.Write("Nhap ID: ");
+            int id = int.Parse(Console.ReadLine());
+            Console.Write("Nhap ten: ");
+            string ten = Console.ReadLine();
+            Console.Write("Nhap gioi tinh: ");
+            string gioiTinh = Console.ReadLine();
+            Console.Write("Nhap tuoi: ");
+            int tuoi = int.Parse(Console.ReadLine());
+            Console.Write("Nhap diem toan: ");
+            double diemToan = double.Parse(Console.ReadLine());
+            Console.Write("Nhap diem ly: ");
+            double diemLy = double.Parse(Console.ReadLine());
+            Console.Write("Nhap diem hoa: ");
+            double diemHoa = double.Parse(Console.ReadLine());
+
+            Students sv = new Students()
+            {
+                id = id,
+                ten = ten,
+                gioiTinh = gioiTinh,
+                tuoi = tuoi,
+                diemToan = diemToan,
+                diemLy = diemLy,
+                diemHoa = diemHoa
+            };
+            sv.TinhDiemTrungBinh();
+            sv.TinhHocLuc();
+            danhSachSinhVien.Add(sv);
+            Console.WriteLine("Them sinh vien thanh cong!");
+        }
+
+        public void CapNhatSinhVien()
+        {
+            Console.Write("Nhap ID sinh vien can cap nhatt: ");
+            int id = int.Parse(Console.ReadLine());
+            Students sv = danhSachSinhVien.Find(x => x.id == id);
+            if (sv != null)
+            {
+                Console.Write("Nhap ten: ");
+                sv.ten = Console.ReadLine();
+                Console.Write("Nhap gioi tinh: ");
+                sv.gioiTinh = Console.ReadLine();
+                Console.Write("Nhap tuoi: ");
+                sv.tuoi = int.Parse(Console.ReadLine());
+                Console.Write("Nhap diem toan: ");
+                sv.diemToan = double.Parse(Console.ReadLine());
+                Console.Write("Nhap diem ly: ");
+                sv.diemLy = double.Parse(Console.ReadLine());
+                Console.Write("Nhap diem hoa: ");
+                sv.diemHoa = double.Parse(Console.ReadLine());
+                sv.TinhDiemTrungBinh();
+                sv.TinhHocLuc();
+                Console.WriteLine("Cap nhat thong tin sinh vien thanh cong!");
+            }
+            else
+            {
+                Console.WriteLine("Khong tim thay sinh vien!");
+            }
+        }
+
+        public void XoaSinhVien()
+        {
+            Console.Write("Nhap ID sinh vien can xoa: ");
+            int id = int.Parse(Console.ReadLine());
+            Students sv = danhSachSinhVien.Find(x => x.id == id);
+            if (sv != null)
+            {
+                danhSachSinhVien.Remove(sv);
+                Console.WriteLine("Xoa sinh vien thanh cong!");
+            }
+            else
+            {
+                Console.WriteLine("Khong tim thay sinh vien!");
+            }
+        }
+
+        public void TimKiemSinhVien()
+        {
+            Console.Write("Nhap ten sinh vien can tim: ");
+            string ten = Console.ReadLine();
+            List<Students> dsTimKiem = danhSachSinhVien.Where(x => x.ten.Contains(ten)).ToList();
+            if (dsTimKiem.Count > 0)
+            {
+                Console.WriteLine("Danh sach sinh vien:");
+                Console.WriteLine("ID \tTen \tGioi tinh \tTuoi \tToan \tLy \tHoa \tĐiem TB \tHoc luc");
+                foreach (Students sv in dsTimKiem)
+                {
+                    Console.WriteLine(sv.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Khong tim thay sinh vien!");
+            }
+        }
+
+        public void SapXepTheoDiemTrungBinh()
+        {
+            danhSachSinhVien = danhSachSinhVien.OrderByDescending(x => x.diemTrungBinh).ToList();
+            Console.WriteLine("Sap xep thanh cong!");
+        }
+
+        public void SapXepTheoTen()
+        {
+            danhSachSinhVien = danhSachSinhVien.OrderBy(x => x.ten).ToList();
+            Console.WriteLine("Sap xep thanh cong!");
+        }
+
+        public void SapXepTheoID()
+        {
+            danhSachSinhVien = danhSachSinhVien.OrderBy(x => x.id).ToList();
+            Console.WriteLine("Sap xep thanh cong!");
+        }
+
+        public void HienThiDanhSachSinhVien()
+        {
+            Console.WriteLine("Danh sach sinh vien:");
+            Console.WriteLine("ID \tTen \tGioi tinh \tTuoi \tToan \tLy \tHoa \tĐiem TB \tHoc luc");
+            foreach (Students sv in danhSachSinhVien)
+            {
+                Console.WriteLine(sv.ToString());
+            }
+        }
+    }
+}
